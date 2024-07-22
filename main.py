9cec9b5c3eaebea60111a3935c4905dd986a9c4f
Cryptic1526 <119151348+Cryptic1526@users.noreply.github.com>
2024-07-23 00:13:23 +1200

    made it look a bit more modern

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from PyQt5.QtSvg import QSvgWidget
+
+class ThreeDotsMenu(QMenu):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.dark_mode_action = QAction("Dark Mode", self, checkable=True)
+        self.addAction(self.dark_mode_action)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,13 +21,18 @@ class MainWindow(QMainWindow):
         self.browser.setUrl(QUrl('https://duckduckgo.com/'))
         
         self.tab_widget = QTabWidget()
-        self.tab_widget.addTab(self.browser, "Loading...")
+        self.tab_widget.setTabsClosable(True)
+        self.tab_widget.tabCloseRequested.connect(self.close_tab)
+        self.tab_widget.addTab(self.browser, "New Tab")
         
         layout = QVBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.tab_widget)
         
         navbar = QToolBar()
-        navbar.setStyleSheet("padding-left: 10px; padding-right: 10px; padding-top: 5px; padding-bottom: 5px;")
+        navbar.setMovable(False)
+        navbar.setIconSize(QSize(16, 16))
         layout.addWidget(navbar)
         
         central_widget = QWidget()
@@ -29,44 +41,59 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
         self.showMaximized()
 
-        back_btn = QAction('Back', self)
+        back_btn = QAction(self.create_svg_icon("./icons/back.svg"), 'Back', self)
         back_btn.triggered.connect(lambda: self.current_web_view().back() if self.current_web_view() else None)
         navbar.addAction(back_btn)
 
-        forward_btn = QAction('Forward', self)
+        forward_btn = QAction(self.create_svg_icon("./icons/forward.svg"), 'Forward', self)
         forward_btn.triggered.connect(lambda: self.current_web_view().forward() if self.current_web_view() else None)
         navbar.addAction(forward_btn)
 
-        refresh_btn = QAction('Refresh', self)
+        refresh_btn = QAction(self.create_svg_icon("./icons/refresh.svg"), 'Refresh', self)
         refresh_btn.triggered.connect(lambda: self.current_web_view().reload() if self.current_web_view() else None)
         navbar.addAction(refresh_btn)
 
-        home_btn = QAction('Home', self)
+        home_btn = QAction(self.create_svg_icon("./icons/home.svg"), 'Home', self)
         home_btn.triggered.connect(self.navigate_home)
         navbar.addAction(home_btn)
 
-        add_tab_btn = QAction('Add Tab', self)
+        navbar.addSeparator()
+
+        self.url_bar = QLineEdit()
+        self.url_bar.setPlaceholderText("Search the web or type a URL")
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+        add_tab_btn = QAction(self.create_svg_icon("./icons/add_tab.svg"), 'New Tab', self)
         add_tab_btn.triggered.connect(self.add_new_tab)
         navbar.addAction(add_tab_btn)
 
-        remove_tab_btn = QAction('Remove Current Tab', self)
-        remove_tab_btn.triggered.connect(self.remove_current_tab)
-        navbar.addAction(remove_tab_btn)
+        self.three_dots_menu = ThreeDotsMenu(self)
+        three_dots_btn = QToolButton(self)
+        three_dots_btn.setIcon(self.create_svg_icon("./icons/three_dots.svg"))
+        three_dots_btn.setPopupMode(QToolButton.InstantPopup)
+        three_dots_btn.setMenu(self.three_dots_menu)
+        navbar.addWidget(three_dots_btn)
 
-        self.url_bar = QLineEdit()
-        self.url_bar.setPlaceholderText("Enter search query or URL")
-        self.url_bar.returnPressed.connect(self.navigate_to_url)
-        navbar.addWidget(self.url_bar)
-        self.url_bar.setStyleSheet("padding: 5px; border: 1px solid black; border-radius: 10px;")
+        self.three_dots_menu.dark_mode_action.triggered.connect(self.toggle_dark_mode)
 
         self.web_views = {0: self.browser}
         self.web_pages = {}
 
         self.setup_shortcuts()
+        self.dark_mode = False
+
+    def create_svg_icon(self, path):
+        svg_widget = QSvgWidget(path)
+        svg_widget.setFixedSize(QSize(16, 16))
+        pixmap = QPixmap(svg_widget.size())
+        pixmap.fill(Qt.transparent)
+        svg_widget.render(pixmap)
+        return QIcon(pixmap)
 
     def setup_shortcuts(self):
         close_tab_shortcut = QShortcut(QKeySequence("Ctrl+W"), self)
-        close_tab_shortcut.activated.connect(self.remove_current_tab)
+        close_tab_shortcut.activated.connect(self.close_current_tab)
 
         open_tab_shortcut = QShortcut(QKeySequence("Ctrl+T"), self)
         open_tab_shortcut.activated.connect(self.add_new_tab)
@@ -77,7 +104,7 @@ class MainWindow(QMainWindow):
     def navigate_to_url(self):
         url = self.url_bar.text().strip()
         if not url.startswith(("http://", "https://")):
-            search_url = f"https://duckduckgo.com/?q={url}"
+            search_url = f"https://www.google.com/search?q={url}"
             self.current_web_view().setUrl(QUrl(search_url))
         else:
             self.current_web_view().setUrl(QUrl(url))
@@ -91,7 +118,7 @@ class MainWindow(QMainWindow):
         new_page = QWebEnginePage(new_web_view)
         new_page.titleChanged.connect(self.update_title)
         new_web_view.setPage(new_page)
-        index = self.tab_widget.addTab(new_web_view, "+")
+        index = self.tab_widget.addTab(new_web_view, "New Tab")
         self.tab_widget.setCurrentIndex(index)
         self.web_views[index] = new_web_view
         self.web_pages[index] = new_page
@@ -99,14 +126,16 @@ class MainWindow(QMainWindow):
         new_web_view.loadStarted.connect(self.clear_url_bar)
         new_web_view.loadFinished.connect(self.update_url_bar_with_current_page_url)
 
-    def remove_current_tab(self):
+    def close_tab(self, index):
         if self.tab_widget.count() > 1:
-            index = self.tab_widget.currentIndex()
             self.tab_widget.removeTab(index)
             del self.web_views[index]
             del self.web_pages[index]
         else:
-            sys.exit()
+            self.close()
+
+    def close_current_tab(self):
+        self.close_tab(self.tab_widget.currentIndex())
 
     def current_web_view(self):
         index = self.tab_widget.currentIndex()
@@ -123,52 +152,134 @@ class MainWindow(QMainWindow):
         url = self.current_web_view().url().toString()
         self.url_bar.setText(url)
 
-app = QApplication(sys.argv)
+    def toggle_dark_mode(self):
+        self.dark_mode = not self.dark_mode
+        self.apply_theme()
 
-# app.setStyleSheet("""
-#     QMainWindow {
-#         background-color: #07090D; 
-#         border-radius: 10px;
-#     }
-#     QToolBar {
-#         background-color: #0E121B;
-#         color: white;
-#         border-radius: 5px; 
-#     }
-#     QPushButton, QLineEdit {
-#         border: none;
-#         color: white;
-#         background-color: #1C2435; 
-#         padding: 5px;
-#         border-radius: 10px;
-#     }
-#     QPushButton:hover, QLineEdit:focus {
-#         background-color: #2F3640;
-#     }
-#     QTabWidget {
-#         background-color: #07090D;
-#         color: white;
-#     }
-#     QTabBar::tab {
-#         background-color: #07090D;
-#         color: white;
-#         padding: 10px;
-#         border-width: 2px; 
-#         border-style: solid;
-#         border-color: #0E121B;
-#         border-radius: 10px;
-#     }
-#     QTabBar::tab:selected {
-#         background-color: #0E121B;
-#     }
-#     QToolBar::action {
-#         color: white; 
-#     }
-#     QToolBar::action:hover {
-#         background-color: #1C2435; /* Button color on hover */
-#     }
-# """)
+    def apply_theme(self):
+        if self.dark_mode:
+            self.setStyleSheet(self.get_dark_theme())
+        else:
+            self.setStyleSheet(self.get_light_theme())
+
+    def get_light_theme(self):
+        return """
+            QMainWindow {
+                background-color: #f0f0f0;
+            }
+            QTabWidget::pane {
+                border: none;
+            }
+            QTabWidget::tab-bar {
+                alignment: left;
+            }
+            QTabBar::tab {
+                background-color: #e0e0e0;
+                color: #5f6368;
+                min-width: 8ex;
+                min-height: 2.5ex;
+                padding: 6px 12px;
+                border: none;
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+                margin-right: 2px;
+            }
+            QTabBar::tab:selected, QTabBar::tab:hover {
+                background-color: #ffffff;
+                color: #202124;
+            }
+            QTabBar::close-button {
+                image: url(./icons/close.svg);
+                subcontrol-position: right;
+            }
+            QToolBar {
+                background-color: #ffffff;
+                border: none;
+                padding: 5px;
+            }
+            QToolBar QToolButton {
+                background-color: transparent;
+                border: none;
+                border-radius: 4px;
+                padding: 4px;
+            }
+            QToolBar QToolButton:hover {
+                background-color: #e0e0e0;
+            }
+            QLineEdit {
+                background-color: #f1f3f4;
+                border: 1px solid #f1f3f4;
+                border-radius: 18px;
+                padding: 5px 10px;
+                selection-background-color: #cfe4fc;
+            }
+            QLineEdit:focus {
+                background-color: #ffffff;
+                border: 1px solid #4285f4;
+            }
+        """
+
+    def get_dark_theme(self):
+        return """
+            QMainWindow {
+                background-color: #202124;
+            }
+            QTabWidget::pane {
+                border: none;
+            }
+            QTabWidget::tab-bar {
+                alignment: left;
+            }
+            QTabBar::tab {
+                background-color: #3c4043;
+                color: #9aa0a6;
+                min-width: 8ex;
+                min-height: 2.5ex;
+                padding: 6px 12px;
+                border: none;
+                border-top-left-radius: 8px;
+                border-top-right-radius: 8px;
+                margin-right: 2px;
+            }
+            QTabBar::tab:selected, QTabBar::tab:hover {
+                background-color: #292b2e;
+                color: #e8eaed;
+            }
+            QTabBar::close-button {
+                image: url(./icons/close_dark.svg);
+                subcontrol-position: right;
+            }
+            QToolBar {
+                background-color: #292b2e;
+                border: none;
+                padding: 5px;
+            }
+            QToolBar QToolButton {
+                background-color: transparent;
+                border: none;
+                border-radius: 4px;
+                padding: 4px;
+            }
+            QToolBar QToolButton:hover {
+                background-color: #3c4043;
+            }
+            QLineEdit {
+                background-color: #3c4043;
+                border: 1px solid #3c4043;
+                border-radius: 18px;
+                padding: 5px 10px;
+                color: #e8eaed;
+                selection-background-color: #8ab4f8;
+            }
+            QLineEdit:focus {
+                background-color: #202124;
+                border: 1px solid #8ab4f8;
+            }
+        """
+
+app = QApplication(sys.argv)
 
 QApplication.setApplicationName("CrypBrowser")
 window = MainWindow()
+window.apply_theme()
 app.exec_()
